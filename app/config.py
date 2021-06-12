f5afa33fdf0cb10619bb77ffb28c9a96478f0b70
Robert Benke <robert@pushpushgo.com>
2021-06-12 10:03:37 +0200

    config.py

---
+++ app/config.py
@@ -0,0 +1,13 @@
+# parametry manipulacyjne
+PPD_SV_range = [1900, 3500] # przepływ powietrza dystrybucyjnego (Nm3/h)
+PPD_SV_MaxStepPerSec = 80.0
+TwDP_SV_range = [65, 81] # tlen w dmuchu procesowym (%)
+TwDP_SV_MaxStepPerSec = 0.013
+PD_SV_range = [40, 70] # prędkość dmuchu [m/s]
+PD_SV_MaxStepPerSec = 2.0
+# parametry manipulacyjne nieuzywane
+NPP_SV_range = [13,27] # nadawa pyłów procesowych
+NPP_SV_MaxStepPerSec = 0
+
+# Cele:
+SCnS_optimal = 25 #strata cieplna na szybie reakcyjnym
